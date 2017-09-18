7c4be65db9ef1d86663fa41fec28e339aa153240
Vickaaparicio <vickaaparicio@gmail.com>
2017-09-18 10:28:30 -0500

    eliminacion de banner y edición de contacto

---
+++ skeleton/contacto.php
@@ -0,0 +1,18 @@
+<!--start-contact-->
+			<div class="contact" id="contact">
+				<div class="wrap">
+					<div class="contact-head">
+						<h3>Contact us</h3>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed et quam est. Mauris faucibus tellus ac auctor posuere.</p>	
+					</div>
+					<div class="contatct-form">
+						<form>
+							<input type="text" value="Name :" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Name :';}">
+							<input type="text" value="Email :" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Email :';}">
+							<textarea rows="2" cols="70" onfocus="if(this.value == 'Message :') this.value='';" onblur="if(this.value == '') this.value='Message :';" >Message :</textarea>
+							<input type="submit" value="Send" />
+						</form>
+					</div>
+				</div>
+			</div>
+			<!---End-contact-->
