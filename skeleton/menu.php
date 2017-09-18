0575cfb07727e2d69be8f1c2aaf764c659f0e700
ElisaApa <32068686+ElisaApa@users.noreply.github.com>
2017-09-18 10:32:12 -0500

    Cambios menu

---
+++ skeleton/menu.php
@@ -0,0 +1,68 @@
+<!---start-wrap-->
+			<!--start-header-->
+			<div class="header" id="home">
+				<div class="wrap">
+				<div class="top-header">
+					<div class="logo">
+						<a href="index.html"><span> </span>Colorful Flat</a>
+					</div>
+					<div class="top-nav">
+						<ul>
+							<li class="active"><a href="#home" class="scroll">Home</a></li>
+							<li><a href="#services" class="scroll">Services</a></li>
+							<li><a href="#portfolio" class="scroll">Portfolio</a></li>
+							<li><a href="#blog" class="scroll">Blog</a></li>
+							<li><a href="#contact" class="scroll">Contact</a></li>
+							<div class="clear"> </div>
+						</ul>
+					</div>
+					<div class="clear"> </div>
+				</div>
+			<!---End-header-->
+			<!--start-content-slider-->
+			<div id="da-slider" class="da-slider">
+					  <div id="intro" class="da-slide">
+					    <div class="da-title">
+					      <h3> the template<br />
+					      	 is a awesome!</h3>
+					    </div>
+					    <div class="da-img">
+					      <img src=" images/slider-pic1.png" alt="Drupal Powered" title="Presenting the Drupalien" />
+					    </div>
+					  </div>
+					  <div id="user-experience" class="da-slide">
+					   <div class="da-title">
+					   <h3> the template<br />
+					      	 is a awesome!</h3>
+					    </div>
+					     <div class="da-img">
+					      <img src=" images/slider-pic2.png" alt="Drupal Powered" title="Presenting the Drupalien" />
+					    </div>
+					  </div>
+					  <div id="design" class="da-slide">
+					   <div class="da-title">
+					    <h3> the template<br />
+					      	 is a awesome!</h3>
+					    </div>
+					     <div class="da-img">
+					      <img src=" images/slider-pic3.png" alt="Drupal Powered" title="Presenting the Drupalien" />
+					    </div>
+					  </div>
+					  <div>
+					  </div>
+					</div>
+					<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+					<script src=" js/jquery.cslider.js"></script>
+					<script>
+					//jQuery(document).ready(function() {
+					$('#da-slider').cslider({
+					  autoplay : true,
+					  interval: 3000,
+					  bgincrement : 10
+					});
+					//});
+					</script>
+		<!----start-content-slider---->
+			</div>
+		</div>
+		<!---End-header----->
