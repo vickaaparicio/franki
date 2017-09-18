d7aebc190c2ddbbec208571f5ffd8a1802fe4191
ElisaApa <32068686+ElisaApa@users.noreply.github.com>
2017-09-18 10:49:15 -0500

    Separaración galeria

---
+++ skeleton/filas.php
@@ -0,0 +1,61 @@
+				<!---start-gallery-script-->
+					<script type="text/javascript" src="js/jquery.easing.min.js"></script>	
+					<script type="text/javascript" src="js/jquery.mixitup.min.js"></script>
+					<script type="text/javascript">
+					$(function () {
+						
+						var filterList = {
+						
+							init: function () {
+							
+								// MixItUp plugin
+								// http://mixitup.io
+								$('#portfoliolist').mixitup({
+									targetSelector: '.portfolio',
+									filterSelector: '.filter',
+									effects: ['fade'],
+									easing: 'snap',
+									// call the hover effect
+									onMixEnd: filterList.hoverEffect()
+								});				
+							
+							},
+							
+							hoverEffect: function () {
+							
+								// Simple parallax effect
+								$('#portfoliolist .portfolio').hover(
+									function () {
+										$(this).find('.label').stop().animate({bottom: 0}, 200, 'easeOutQuad');
+										$(this).find('img').stop().animate({top: -30}, 500, 'easeOutQuad');				
+									},
+									function () {
+										$(this).find('.label').stop().animate({bottom: -40}, 200, 'easeInQuad');
+										$(this).find('img').stop().animate({top: 0}, 300, 'easeOutQuad');								
+									}		
+								);				
+							}
+						};
+						// Run the show!
+						filterList.init();
+					});	
+					</script>
+					<!-- Add fancyBox main JS and CSS files -->
+					<script src="js/jquery.magnific-popup.js" type="text/javascript"></script>
+					<link href="css/magnific-popup.css" rel="stylesheet" type="text/css">
+							<script>
+								$(document).ready(function() {
+									$('.popup-with-zoom-anim').magnificPopup({
+										type: 'inline',
+										fixedContentPos: false,
+										fixedBgPos: true,
+										overflowY: 'auto',
+										closeBtnInside: true,
+										preloader: false,
+										midClick: true,
+										removalDelay: 300,
+										mainClass: 'my-mfp-zoom-in'
+								});
+							});
+							</script>
+				<!---//End-gallery-script-->
