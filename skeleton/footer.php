94b4ab2e36aba894bbb0efb08d334264532f22f3
Vickaaparicio <vickaaparicio@gmail.com>
2017-09-18 10:44:57 -0500

    separación index y footer

---
+++ skeleton/footer.php
@@ -0,0 +1,40 @@
+<!---start-footer-->
+		<div class="footer">
+			<div class="wrap">
+				<div class="footer-grids">
+					<div class="footer-left">
+						<ul>
+							<li><a class="ftwiter" href="#"> </a></li>
+							<li><a class="fface" href="#"> </a></li>
+							<li><a class="fhtml" href="#"> </a></li>
+							<li><a class="fflick" href="#"> </a></li>
+							<div class="clear"> </div>
+						</ul>
+					</div>
+					<div class="footer-right">
+						<p>Template by <a href="http://w3layouts.com/">W3layouts</a></p>
+									<script type="text/javascript">
+						$(document).ready(function() {
+							$().UItoTop({ easingType: 'easeOutQuart' });
+						});
+					</script>
+					<!----move-top-path---->
+					<script type="text/javascript" src="js/move-top.js"></script>
+					<script type="text/javascript" src="js/easing.js"></script>
+					<script type="text/javascript">
+						jQuery(document).ready(function($) {
+							$(".scroll").click(function(event){		
+								event.preventDefault();
+								$('html,body').animate({scrollTop:$(this.hash).offset().top},1200);
+							});
+						});
+					</script>
+					<!----move-top-path---->
+			    <a href="#" id="toTop" style="display: block;"><span id="toTopHover" style="opacity: 1;"></span></a>
+
+					</div>
+					<div class="clear"> </div>
+				</div>
+			</div>
+		</div>
+		<!----//End-footer---->
