3c48e371c044238ead8bf10e859476867038cda2
Vickaaparicio <vickaaparicio@gmail.com>
2017-09-18 10:49:03 -0500

    separación index y redes sociales

---
+++ skeleton/redessociales.php
@@ -0,0 +1,56 @@
+<!---start-meet-our-team-->
+			<div class="team">
+				<div class="wrap">
+					<div class="team-head">
+						<h3>Meet Our team</h3>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed et quam est. Mauris faucibus tellus ac auctor posuere.</p>
+					</div>
+					<div class="team-members">
+						<div class="team-member">
+							<span><a href="#"> </a></span>
+							<h4><a href="#">John Smith</a></h4>
+						   <label>Creative Director</label>
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed et quam est.</p>
+							<ul>
+								<li>
+									<li><a class="facebook simptip-position-bottom simptip-movable" data-tooltip="Facebook" href="#"> </a></li>
+									<li><a class="twitter simptip-position-bottom simptip-movable" data-tooltip="Twitter" href="#"> </a></li>
+									<li><a class="pin simptip-position-bottom simptip-movable" data-tooltip="Pintrest" href="#"> </a></li>
+									<div class="clear"> </div>
+								</li>
+							</ul>
+						</div>
+						<div class="team-member">
+							<span><a href="#"> </a></span>
+							<h4><a href="#">John Smith</a></h4>
+						   <label>Creative Director</label>
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed et quam est.</p>
+							<ul>
+								<li>
+									<li><a class="facebook simptip-position-bottom simptip-movable" data-tooltip="Facebook" href="#"> </a></li>
+									<li><a class="twitter simptip-position-bottom simptip-movable" data-tooltip="Twitter" href="#"> </a></li>
+									<li><a class="pin simptip-position-bottom simptip-movable" data-tooltip="Pintrest" href="#"> </a></li>
+									<div class="clear"> </div>
+								</li>
+							</ul>
+						</div>
+						<div class="team-member">
+							<span><a href="#"> </a></span>
+							<h4><a href="#">John Smith</a></h4>
+						   <label>Creative Director</label>
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed et quam est.</p>
+							<ul>
+								<li>
+									<li><a class="facebook simptip-position-bottom simptip-movable" data-tooltip="Facebook" href="#"> </a></li>
+									<li><a class="twitter simptip-position-bottom simptip-movable" data-tooltip="Twitter" href="#"> </a></li>
+									<li><a class="pin simptip-position-bottom simptip-movable" data-tooltip="Pintrest" href="#"> </a></li>
+									<div class="clear"> </div>
+								</li>
+							</ul>
+						</div>
+						<div class="clear"> </div>
+						<div class="clear"> </div>
+					</div>
+				</div>
+			</div>
+			<!---//End-meet-our-team-->
