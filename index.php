8e597e05b37a7b9586a16dfa34a85b518eb893ed
ElisaApa <32068686+ElisaApa@users.noreply.github.com>
2017-09-23 09:35:54 -0500

    Separacion blog y posts

--- index.php
+++ index.php
@@ -33,70 +33,11 @@
 			<!---Galeria---->
 			<?php include"skeleton/galeria.php/";?>
 			<!--- start-blog-->
-			<div class="blog">
-				<div class="wrap">
-					<div class="blog-grids">
-						<div class="blog-grid blog-grid1">
-							<div class="blog-grid-left" onclick="location.href='#';">
-								<span> </span>
-							</div>
-							<div class="blog-grid-right">
-								<h3><a href="#">Layered Neatly Photoshop Template</a></h3>
-								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed et quam est. Mauris faucibus tellus ac auctor posuere. Integer lobortis purus a felis adipiscing, eget ornare justo semper. Etiam commodo tincidunt ante.</p>
-								<a class="blogbtng popup-with-zoom-anim" href="#small-dialog1">Show More</a>
-							</div>
-							<div class="clear"> </div>
-						</div>
-						<!---->
-						<div class="blog-grid2">
-							<div class="blog-grid2-left">
-								<h3><a href="#">Layered Neatly Photoshop Template</a></h3>
-								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed et quam est. Mauris faucibus tellus ac auctor posuere. Integer lobortis purus a felis adipiscing, eget ornare justo semper. Etiam commodo tincidunt ante.</p>
-								<a class="blogbtnb popup-with-zoom-anim" href="#small-dialog1">Show More</a>
-							</div>
-							<div class="blog-grid2-right" onclick="location.href='#';">
-								<span> </span>
-							</div>
-							<div class="clear"> </div>
-						</div>
-						<!---->
-						<div class="clear"> </div>
-					 </div>
-				</div>
-			</div>
+			<?php include"skeleton/blog.php/";?>
 			<!--- End-blog-->
 			<?php include"skeleton/redessociales.php";?>
 			<!--- start-recent-posts-->
-			<div class="recent-posts" id="blog">
-				<div class="wrap">
-				<div class="recent-posts-head">
-					<h3>Recent Posts</h3>
-					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed et quam est. Mauris faucibus tellus ac auctor posuere.</p>	
-				</div>
-				<div class="post-grids">
-					<div class="post-grid">
-						<span onclick="location.href='#';"><a href="#"> </a></span>
-						<h4><a href="#">Really Interesting Post</a></h4>
-						<label>October 27</label>
-						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed et quam est.  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed et quam est.</p>
-					</div>
-					<div class="post-grid">
-						<span onclick="location.href='#';"><a href="#"> </a></span>
-						<h4><a href="#">Really Interesting Post</a></h4>
-						<label>October 27</label>
-						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed et quam est.  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed et quam est.</p>
-					</div>
-					<div class="post-grid">
-						<span onclick="location.href='#';"><a href="#"> </a></span>
-						<h4><a href="#">Really Interesting Post</a></h4>
-						<label>October 27</label>
-						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed et quam est.  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed et quam est.</p>
-					</div>
-					<div class="clear"> </div>
-					<a class="morebtn popup-with-zoom-anim" href="#small-dialog1">Show More</a>
-				</div>
-				</div>
-			</div>
+			<?php include"skeleton/posts.php/";?>
 			<!---//End-recent-posts-->
 			<?php include"skeleton/contacto.php";?>
 		</div>
---
+++ skeleton/posts.php
@@ -0,0 +1,30 @@
+			<div class="recent-posts" id="blog">
+				<div class="wrap">
+				<div class="recent-posts-head">
+					<h3>Recent Posts</h3>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed et quam est. Mauris faucibus tellus ac auctor posuere.</p>	
+				</div>
+				<div class="post-grids">
+					<div class="post-grid">
+						<span onclick="location.href='#';"><a href="#"> </a></span>
+						<h4><a href="#">Really Interesting Post</a></h4>
+						<label>October 27</label>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed et quam est.  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed et quam est.</p>
+					</div>
+					<div class="post-grid">
+						<span onclick="location.href='#';"><a href="#"> </a></span>
+						<h4><a href="#">Really Interesting Post</a></h4>
+						<label>October 27</label>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed et quam est.  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed et quam est.</p>
+					</div>
+					<div class="post-grid">
+						<span onclick="location.href='#';"><a href="#"> </a></span>
+						<h4><a href="#">Really Interesting Post</a></h4>
+						<label>October 27</label>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed et quam est.  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed et quam est.</p>
+					</div>
+					<div class="clear"> </div>
+					<a class="morebtn popup-with-zoom-anim" href="#small-dialog1">Show More</a>
+				</div>
+				</div>
+			</div>
